d11eb713c025639c1559eb702e20c3ad31332f33
timothyclark5628-creator <timothyclark5628@gmail.com>
2026-03-06 13:55:07 -0500

    Mute on throw copied
    Edits to pickupScript.cs were forgotten causing the floating objects to play a sound as if they have bumped into something when picked up and thrown, code has been copied over to fix this again.

--- Assets/Scripts/Audio/FloatingImpactAudio.cs
+++ Assets/Scripts/Audio/FloatingImpactAudio.cs
@@ -71,7 +71,7 @@ public class FloatingImpactAudio : MonoBehaviour
     private IEnumerator bumpCooldown()
     {
         bumpSoundOnCoolDown = true;
-        yield return new WaitForSeconds(0.2f);
+        yield return new WaitForSeconds(0.05f);
         bumpSoundOnCoolDown = false;
     }
 
