0e38c3b436027f2a193ba2b41bfcda250a3c0e4a
noahsark13 <nhk3466@rit.edu>
2026-03-25 00:50:38 -0400

    Bone stretching w IK

---
+++ Assets/Scripts/Rig/StretchIKBones.cs
@@ -0,0 +1,73 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class StretchIKBones : MonoBehaviour
+{
+
+    const float MAX_DISTANCE = 2.5f;
+    private float stretchFactor = 1f;
+
+    [SerializeField]
+    private Transform root;
+    [SerializeField]
+    private Transform target;
+
+    private Vector3 previousPosition;
+    private Quaternion previousRotation;
+
+    private List<Transform> bones;
+    private Vector3[] initPositions;
+
+    [SerializeField]
+    private float distance;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        bones = GetBoneChain(root);
+
+        initPositions = new Vector3[bones.Count];
+        for (int i = 0; i < bones.Count; i++)
+            initPositions[i] = bones[i].localPosition;
+
+
+        distance = Vector3.Distance(root.position, target.position); ;
+        previousPosition = target.position;
+        previousRotation = target.rotation;
+       
+    }
+
+    void Update()
+    {
+        if (target.position != previousPosition)
+        {
+            distance = Vector3.Distance(root.position, target.position);
+            stretchFactor = distance > MAX_DISTANCE ? distance / MAX_DISTANCE : 1f;
+            previousPosition = target.position;
+        }
+
+        for (int i = 1; i < bones.Count; i++)
+            bones[i].localPosition = initPositions[i] * stretchFactor;
+    }
+
+    void LateUpdate()
+    {
+      
+        for (int i = 1; i < bones.Count; i++)
+            bones[i].localPosition = initPositions[i] * stretchFactor;
+    }
+
+    List<Transform> GetBoneChain(Transform root)
+    {
+        List<Transform> chain = new List<Transform>();
+        Transform current = root;
+
+        while (current != null)
+        {
+            chain.Add(current);
+            current = current.childCount > 0 ? current.GetChild(0) : null;
+        }
+
+        return chain;
+    }
+}
