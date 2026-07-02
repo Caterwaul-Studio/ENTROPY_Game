8c63986d578e5f5e72fcb36b9f1c727a8d7a5baa
timothyclark5628-creator <timothyclark5628@gmail.com>
2026-07-02 14:57:59 -0400

    Spray & Puff adjustments
    Puff move speed now adjustable, made faster.
    Spray collisions changed to ignore holdlayer and collision quality lowered.
    Spray starting position moved.

--- Assets/Scripts/Fire/PuffMovement.cs
+++ Assets/Scripts/Fire/PuffMovement.cs
@@ -8,13 +8,14 @@ public class PuffMovement : MonoBehaviour
     [SerializeField] private GameObject player;
     [SerializeField] private Camera MainCamera;
     [SerializeField] private float playerPushForce;
+    [SerializeField] private float puffSpeed;
     public void Shoot(Transform firePosition)
     {
         transform.position = firePosition.transform.position;
         transform.rotation = MainCamera.transform.rotation;
         Vector3 direction = new Vector3(0, 0, 0);
         direction = transform.forward;
-        gameObject.GetComponent<Rigidbody>().linearVelocity = transform.forward;
+        gameObject.GetComponent<Rigidbody>().linearVelocity = transform.forward * puffSpeed;
         gameObject.GetComponent<Rigidbody>().AddForce(player.GetComponent<Rigidbody>().linearVelocity,ForceMode.VelocityChange);
         //player.GetComponent<Rigidbody>().AddForce(player.GetComponent<Rigidbody>().linearVelocity * -playerPushForce, ForceMode.VelocityChange); //slows you down gradually, cant go backwards.
         player.GetComponent<Rigidbody>().AddForce(transform.forward * -playerPushForce, ForceMode.VelocityChange);
